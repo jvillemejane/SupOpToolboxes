69825bcfc11b6d84be293a5c2946e58e5f71ced8
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-03-12 14:29:37 +0100

    Updating Physics Demo - FFT signals

--- physics_demo/signals/ft_fft/Demo_signals_fft.py
+++ physics_demo/signals/ft_fft/Demo_signals_fft.py
@@ -9,24 +9,35 @@ Created on 11/mar/2023
 @author: julien.villemejane
 """
 
-from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import (
-    QApplication, QDialog, QMainWindow, QMessageBox, QWidget
-)
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
 
-import sys
 import numpy as np
+from pyqtgraph import PlotWidget, plot, mkPen
 from signal_processing.signal_processing import generate_sinus_time
+
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+
 
 """
 MainWindow class
 """
+class MainWindow(QMainWindow):
 
-class MainWindow(QtWidgets.QMainWindow):
+    
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.plotSignalWidget = PlotWidget()
+        
+        
+        loadUi("Demo_signals_fft.ui", self)
+
+
+"""
+MainWindow class
+"""
+class MainWindow(QMainWindow):
 
     
     def __init__(self):
@@ -34,22 +45,21 @@ class MainWindow(QtWidgets.QMainWindow):
         loadUi("Demo_signals_fft.ui", self)
         
         """ Signal Widget """
-        self.plotSignalWidget = pg.PlotWidget(title='Time-dependent signal ')
+        self.plotSignalWidget = PlotWidget(title='Time-dependent signal ')
         self.leftLayout.addWidget(self.plotSignalWidget)
+        self.plotSignalWidget.setBackground('w')
+        
         self.sin1Widget.setStyleSheet("background-color:lightblue;");
         self.sin2Widget.setStyleSheet("background-color:lightgreen;");
 
-        self.plotSignalWidget.setBackground('w')
         #self.plotSignalWidget.setYRange(0, 1, padding=0)
 
-        self.pen = pg.mkPen(color=(255, 0, 0), width=5)
+        self.pen = mkPen(color=(255, 0, 0), width=5)
         
         """ FFT Widget """
-        self.plotFFTWidget = pg.PlotWidget(title='Fourier Transform / Frequency-dependent')
+        self.plotFFTWidget = PlotWidget(title='Fourier Transform / Frequency-dependent')
         self.rightLayout.addWidget(self.plotFFTWidget)
-        self.plotFFTWidget.setBackground('w')
-        
-        self.pen = pg.mkPen(color=(255, 0, 0), width=5)
+        self.plotFFTWidget.setBackground('w')     
         
         """ Sampling Widget """
         self.sampFreqValues = ['125', '250', '500', '1000', '5000', '10000']
@@ -74,7 +84,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.time, self.signal, self.freq, self.s_fft = self.generate_signals()
         self.plotSig = self.plotSignalWidget.plot(self.time, self.signal, pen=self.pen)
         self.plotFFT = self.plotFFTWidget.plot(self.freq, self.s_fft, pen=self.pen)
-
+        
         """ Events """        
         self.refreshPlotsBt.clicked.connect(self.refreshGraph)
         self.freq1Slider.valueChanged.connect(self.freqChanged)
@@ -84,7 +94,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.samplesCombo.currentIndexChanged.connect(self.freqChanged)
         
         self.refreshGraph()
-
 
     def freqChanged(self):
         self.refreshGraph()
@@ -111,11 +120,11 @@ class MainWindow(QtWidgets.QMainWindow):
         #self.freq = self.freq[freq_half-f_range:freq_half+f_range]
         #self.s_fft = self.s_fft[freq_half-f_range:freq_half+f_range]
         """ Displaying data """
+        
         self.plotSignalWidget.removeItem(self.plotSig)
         self.plotSig = self.plotSignalWidget.plot(self.time, self.signal, pen=self.pen)
         self.plotFFTWidget.removeItem(self.plotFFT)
         self.plotFFT = self.plotFFTWidget.plot(self.freq, self.s_fft, pen=self.pen)
-
     
     def generate_signals(self):
         final_time = self.samples / self.sampling_freq
