4b3754f34ece536e4efd6239e6e8b39cc21b3da9
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-03-22 15:07:38 +0100

    Adding Physics Demo / FFT Convolution / Signals

--- physics_demo/signals/ft_fft_convolution/Demo_signals_fft_convolution.py
+++ physics_demo/signals/ft_fft_convolution/Demo_signals_fft_convolution.py
@@ -29,111 +29,89 @@ class MainWindow(QMainWindow):
     
     def __init__(self):
         super().__init__(parent=None)
-        loadUi("Demo_signals_fft.ui", self)
+        loadUi("Demo_signals_fft_convolution.ui", self)
+        
+        """ """
+        self.maxNumber = 1001
+        self.offsetSlider.setMinimum(-100)
+        self.offsetSlider.setMaximum(100)
+        self.offsetSlider.setValue(0)
+        self.offset = self.offsetSlider.value()
+        self.offsetValue.setText(f'{self.offset}')
+        self.numberSlider.setMinimum(10)
+        self.numberSlider.setMaximum(self.maxNumber//20)
+        self.numberSlider.setValue(self.maxNumber//20)
+        self.number = self.numberSlider.value()
+        self.numberValue.setText(f'{self.number}')
         
         """ Signal Widget """
-        self.plotSignalWidget = PlotWidget(title='Time-dependent signal ')
-        self.leftLayout.addWidget(self.plotSignalWidget)
+        self.plotSignalWidget = PlotWidget(title='Sinus Cardinal')
+        self.signalLayout.addWidget(self.plotSignalWidget)
         self.plotSignalWidget.setBackground('w')
+        self.plotSignalWidget.setYRange(-0.3, 1.1)
         
-        self.sin1Widget.setStyleSheet("background-color:lightblue;");
-        self.sin2Widget.setStyleSheet("background-color:lightgreen;");
-
-        self.pen = mkPen(color=(128, 128, 0), width=5)
-        
-        """ FFT Widget """
-        self.plotFFTWidget = PlotWidget(title='Fourier Transform / Frequency-dependent')
-        self.rightLayout.addWidget(self.plotFFTWidget)
-        self.plotFFTWidget.setBackground('w')        
-        
-        max_freq = self.freq1Slider.maximum()
-        self.plotFFTWidget.setXRange(-max_freq, max_freq, padding=0)  
-
-        self.plotFFTWidget.setYRange(-.1, 1.1, padding=0)
-        
-        """ Sampling Widget """
-        self.sampFreqValues = ['125', '250', '500', '1000', '5000', '10000']
-        self.samplingFreqCombo.addItems(self.sampFreqValues)
-        self.samplesValues = ['10000', '5000', '1000', '500', '100', '50', '10']
-        self.samplesCombo.addItems(self.samplesValues)
-            
-        """ Initial values for data generation """
-        self.samples = 2000
-        self.sampling_freq = 1000
-        self.sin_freq1 = 20
-        self.sin_freq2 = 0
-        self.freq1Slider.setValue(self.sin_freq1)
-        self.freq1Value.setText(f'{self.sin_freq1} Hz')
-        if(self.onSig2.isChecked()):
-            self.freq2Slider.setValue(self.sin_freq2)
-            self.freq2Value.setText(f'{self.sin_freq2} Hz')
-        else:
-            self.freq2Value.setText(f'OFF')
-        
-        """ Signals generation"""
-        self.time, self.signal, self.freq, self.s_fft = self.generate_signals()
-        self.plotSig = self.plotSignalWidget.plot(self.time, self.signal, pen=self.pen)
-        self.plotFFT = self.plotFFTWidget.plot(self.freq, self.s_fft, pen=self.pen)
+        self.offsetWidget.setStyleSheet("background-color:lightblue;");
+        self.samplesWidget.setStyleSheet("background-color:lightgreen;");
         
         """ Events """        
-        self.refreshPlotsBt.clicked.connect(self.refreshGraph)
-        self.freq1Slider.valueChanged.connect(self.freqChanged)
-        self.freq2Slider.valueChanged.connect(self.freqChanged)
-        self.onSig2.stateChanged.connect(self.freqChanged)
+        self.offsetSlider.valueChanged.connect(self.freqChanged)
+        self.numberSlider.valueChanged.connect(self.freqChanged)
+        self.onSig.stateChanged.connect(self.freqChanged)
+        self.absSig.stateChanged.connect(self.freqChanged)
         self.samplingFreqCombo.currentIndexChanged.connect(self.freqChanged)
         self.samplesCombo.currentIndexChanged.connect(self.freqChanged)
+        self.resetBt.clicked.connect(self.resetGraph)
         
         self.refreshGraph()
 
     def freqChanged(self):
         self.refreshGraph()
         
+    def resetGraph(self):
+        self.offset = 0
+        self.offsetValue.setText(f'{self.offset}')
+        self.offsetSlider.setValue(self.offset)
+        self.number = self.maxNumber // 20
+        self.numberValue.setText(f'{self.number}')
+        self.numberSlider.setValue(self.number)
+        self.absSig.setChecked(False)
+        self.onSig.setChecked(False)
+        self.refreshGraph()
+        
         
     def refreshGraph(self):
         """ Collecting new values """        
-        self.sampling_freq = int(self.samplingFreqCombo.currentText())
-        self.samples = int(self.samplesCombo.currentText())
-        self.sin_freq1 = self.freq1Slider.value()
-        self.freq1Value.setText(f'{self.sin_freq1} Hz')
-        if(self.onSig2.isChecked()):
-            self.sin_freq2 = self.freq2Slider.value()
-            self.freq2Value.setText(f'{self.sin_freq2} Hz')
-        else:
-            self.freq2Value.setText(f'OFF')
-            
+        self.offset = (self.offsetSlider.value() / 10.0)
+        self.offsetValue.setText(f'{self.offset}')
+        self.number = self.numberSlider.value()
+        self.numberValue.setText(f'{self.number}')
         
         """ Signals generation"""
-        self.time, self.signal, self.freq, self.s_fft = self.generate_signals()
-        """ Zoom for displaying """
-        freq_half = int(self.samples/2)        
-        max_freq = self.freq1Slider.maximum()
-        f_range = max_freq / self.sampling_freq * self.samples
-        if(f_range > self.samples/2):
-            f_range = int(self.samples/2)
-        else:
-            f_range = int(f_range)
-        
-        self.freq = self.freq[freq_half-f_range:freq_half+f_range]
-        self.s_fft = self.s_fft[freq_half-f_range:freq_half+f_range]
+        self.x, self.signal, self.xFT, self.signalFT = self.generate_signals()
         
         """ Displaying data """
-        self.plotSignalWidget.removeItem(self.plotSig)
-        self.plotSig = self.plotSignalWidget.plot(self.time, self.signal, pen=self.pen)
-        self.plotFFTWidget.removeItem(self.plotFFT)
-        self.plotFFT = self.plotFFTWidget.plot(self.freq, self.s_fft, pen=self.pen)
+        self.plotSignalWidget.clear()
+        penSinC = mkPen(color=(128, 128, 0), width=3)
+        penSinCFT = mkPen(color=(0, 0, 255), width=4)
+        
+        self.plotSignalWidget.plot(self.x, self.signal, pen=penSinC)
+        if(self.onSig.isChecked()):
+            self.plotSignalWidget.plot(self.xFT, self.signalFT, pen=penSinCFT)
     
     def generate_signals(self):
-        final_time = self.samples / self.sampling_freq
-        time = np.linspace(0, final_time, self.samples)
-        
-        dataSig = generate_sinus_time(self.sin_freq1, time)
-        if(self.onSig2.isChecked()):
-            dataSig+= generate_sinus_time(self.sin_freq2, time)
-        
-        freq = np.fft.fftfreq(self.samples, time[1]-time[0])
-        freq = np.fft.fftshift(freq)
-        dataFFT = np.abs(np.fft.fftshift(np.fft.fft(dataSig)))/self.samples
-        return time, dataSig, freq, dataFFT
+        x = np.linspace(-10, 10, self.maxNumber)
+        y = np.sinc(x + self.offset)
+        if(self.absSig.isChecked()):
+            y = np.abs(y)
+        if(self.onSig.isChecked()):
+            x2 = np.linspace(-10, 10, self.number)
+            y2 = np.sinc(x2 + self.offset)
+            if(self.absSig.isChecked()):
+                y2 = np.abs(y2)
+        else:
+            x2 = 0
+            y2 = 0
+        return x, y, x2, y2
     
     def closeEvent(self, event):
         QApplication.quit()
