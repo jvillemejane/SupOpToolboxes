b5312f061af6544f64bdd8975acb4caf76767089
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-03-23 10:24:02 +0100

    Adding Python examples / PyQt5 first example

---
+++ _essais/PyQt5/graphicalLEnsE/graphicalLEnsE.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Signal Processing libraries of functions
+
+Author : Julien VILLEMEJANE
+Laboratoire d Enseignement Experimental - Institut d Optique Graduate School
+Version : 1.0 - 2022-12-01
+"""
+
+
+
+"""
+
+"""
+class sliderBlock:
+    
+    def __init__(self):
+        self.name 
+    
+   
